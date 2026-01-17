a53ac3b2d715a65969595593966dfd62b1ff51bf
Iury Lenon Alves <iuryalves.uk@gmail.com>
2026-01-17 16:19:05 +0000

    feat(analytics): integrate Vercel Analytics and Speed Insights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,10 @@ import { Toaster } from "react-hot-toast";
 import { CookieConsent } from "@/components/ui/cookie-consent"
 import type { Metadata, Viewport } from "next";
 import "../globals.css";
+
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import GoogleAnalytics from '@/components/analytics/GoogleAnalytics';
 import JsonLd from '@/components/seo/JsonLd';
 
@@ -107,8 +111,14 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <body className={`${inter.variable} ${poppins.variable} antialiased font-sans`}>
+        
+        {/*Google Analytics */}
         <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_ID || ''} />
         <JsonLd />
+        {/*Vercel Analytics and Speed Insights */}
+        <Analytics />
+        <SpeedInsights />
+
         <NextIntlClientProvider messages={messages}>
           {children}
           <CookieConsent />
